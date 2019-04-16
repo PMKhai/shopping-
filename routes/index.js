234637149695d2db60901c5f7a4251f86d1443fb
Khai Pham <minhkhai3012@gmail.com>
2019-04-16 13:12:35 +0700

    create register page, info page, forgot password page

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/*GET product page */
+/*GET product page. */
 router.get('/product', (req, res, next) => {
   res.render('product', { title: 'Shop' });
 });
 
+/*Get Login page. */
 router.get('/login', (req, res) => {
-  res.render('loginregister', {title: 'Đăng nhập'});
+  res.render('login', {title: 'Đăng nhập'});
+});
+
+/* Get Register page.*/
+router.get('/register', (req, res) => {
+  res.render('register', {title: 'Tạo tài khoản'});
+});
+
+/* Get Forgot password page.*/
+router.get('/forgot', (req, res) => {
+  res.render('forgotpassword', {title: 'Quên mật khẩu'});
+});
+
+/* Get Info page.*/
+router.get('/info', (req, res) => {
+  res.render('info', {title: 'Thông tin tài khoản'});
 });
 
 module.exports = router;
