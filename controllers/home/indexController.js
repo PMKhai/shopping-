6420d983ebb6630a29c98982790e9486820e3a12
Kha Leo <lexuankha2409@gmail.com>
2019-05-13 01:41:39 +0700

    Load HomePage
    -Load Sản phẩm
    -Sửa lỗi chính tả

--- controllers/home/indexController.js
+++ controllers/home/indexController.js
@@ -1,5 +1,28 @@
-exports.index  = (req, res, next) => {
+const product = require('../../models/index');
+
+exports.index  = async (req, res, next) => {
+    const products = await product.list();
+    console.log(products);
     res.render('home/index', {
-        tile: 'Trang chủ'
+        tile: 'Trang chủ',
+        products
     });
+}
+
+exports.product_list = async (req, res, next) => {
+    const products = await product.list();
+    console.log(products);
+    res.render('home/index', {
+        title: "Cửa hàng",
+        products
+    })
+}
+
+exports.product_detail = async (req, res, next) => {
+    const products = await product.detail(req.params.id);
+    console.log(products);
+    res.render('home/index', {
+        title: products.name,
+        products
+    })
 }
--- routes/home/index.js
+++ routes/home/index.js
@@ -6,4 +6,10 @@ var index_controller = require('../../controllers/home/indexController');
 /* GET home page. */
 router.get('/', index_controller.index);
 
+/* GET product listing. */
+router.get('/', index_controller.product_list);
+
+// /* Get product detail*/
+// router.get('/:id', product_controller.product_detail);
+
 module.exports = router;
