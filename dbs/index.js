d4e683e6e3e4f1b8562700c1a0fa7e579aeac740
Khai Pham <minhkhai3012@gmail.com>
2019-05-13 16:28:30 +0700

    Connect to DB in cloud

--- dbs/index.js
+++ dbs/index.js
@@ -2,8 +2,9 @@ const MongoClient = require('mongodb').MongoClient
 
 // Note: A production application should not expose database credentials in plain text.
 // For strategies on handling credentials, visit 12factor: https://12factor.net/config.
-const PROD_URI = "mongodb://127.0.0.1:27017/data"
 // const MKTG_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>"
+//const PROD_URI = "mongodb://127.0.0.1:27017/data" // Database local
+const PROD_URI = "mongodb+srv://khaipham:123456789a@cluster0-lwsla.mongodb.net/sample_training?retryWrites=true" //Database cloud
 
 var dbs = {
     production: {}
