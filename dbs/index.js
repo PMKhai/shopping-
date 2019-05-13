171876aacf300c9bfb833b1df405085098e63f76
Khai Pham <minhkhai3012@gmail.com>
2019-05-13 19:29:36 +0700

    Edit connection string

--- dbs/index.js
+++ dbs/index.js
@@ -1,24 +1,24 @@
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient;
 
 // Note: A production application should not expose database credentials in plain text.
 // For strategies on handling credentials, visit 12factor: https://12factor.net/config.
 // const MKTG_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>"
-//const PROD_URI = "mongodb://127.0.0.1:27017/data" // Database local
-const PROD_URI = "mongodb+srv://khaipham:123456789a@cluster0-lwsla.mongodb.net/sample_training?retryWrites=true" //Database cloud
+//const PROD_URI = "mongodb://127.0.0.1:27017/data"; // Database local
+const PROD_URI =
+  "mongodb+srv://khaipham:123456789a@cluster0-lwsla.mongodb.net/LTW-data?retryWrites=true"; //Database cloud
 
 var dbs = {
-    production: {}
+  production: {}
 };
 
 function connect(url) {
-    return MongoClient.connect(url).then(client => client.db())
+  return MongoClient.connect(url).then(client => client.db());
 }
 
-exports.initdb = async function () {
-    // let databases = await Promise.all([connect(PROD_URI), connect(MKTG_URI)])
-    let database = await connect(PROD_URI);
-    dbs.production = database;
-}
-
+exports.initdb = async function() {
+  // let databases = await Promise.all([connect(PROD_URI), connect(MKTG_URI)])
+  let database = await connect(PROD_URI);
+  dbs.production = database;
+};
 
 exports.dbs = dbs;
