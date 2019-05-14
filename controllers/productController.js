464fdd2fa6aca4e96e4ff9a54a24f7fcc4c88a59
Huy Nguyễn <huycong2798@gmail.com>
2019-05-14 11:36:11 +0700

    fix linh tinh

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,22 +1,23 @@
 const product = require('../models/product');
 
 exports.product_list = async (req, res, next) => {
-    const products = await product.list();
-    console.log(products);
+    const productdetail = await product.detail(req.params.id);
+    const productList = await product.list();
+    console.log(productdetail);
+    console.log(productList);
     res.render('product/product', {
         title: "Cửa hàng",
-        products
+        productdetail,productList
     })
 }
 
 exports.product_detail = async (req, res, next) => {
-    const products = await product.detail(req.params.id);
+    const productdetail = await product.detail(req.params.id);
     const productsList = await product.list();
-    console.log(products);
+    console.log(productdetail);
     console.log(productsList);
     res.render('product/product-detail', {
-        title: products.name,
-        products,
-        productsList
+        title: productdetail.name,
+        productdetail,productsList
     })
 }
