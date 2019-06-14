c023c274e425d5951d30960301adebb49a3235fb
Huy Nguyễn <huycong2798@gmail.com>
2019-06-14 23:27:05 +0700

    update load list order and detail order

--- controllers/customer/oderController.js
+++ controllers/customer/oderController.js
@@ -1,11 +1,22 @@
+
 exports.oder_list = (req, res, next) => {
+
+    if(!req.user)
+        res.redirect("/login");
+    const listorder = req.user.listorder;
     res.render("customer/oder/myoder", {
-        title: "S",user: req.user
+        title: "S",user: req.user,listorder
     });
 }
 
-exports.oder_detail = (req, res, next) => {
+exports.order_detail = async (req, res, next) => {
+
+    if(!req.user)
+        res.redirect("/login");
+    const detailorder = req.user.listorder[req.params.id];
+
+    console.log(detailorder);
     res.render("customer/oder/detailoder", {
-        title: "Chi Tiết",user: req.user
+        title: "Chi Tiết",user: req.user,detailorder
     });
 }
--- controllers/customer/profileController.js
+++ controllers/customer/profileController.js
@@ -1,4 +1,6 @@
 exports.profile = (req, res, next) => {
+    if(!req.user)
+        res.redirect("/login");
     res.render('customer/profile', {
         tile: 'Thông tin tài khoản',user: req.user
     });
